d723659a1166a6ad19dac40d242525eb3bc4b8a5
Andreas Dellrud <nerendor@hotmail.com>
2017-09-22 12:57:52 +0200

    Added @import to panel as an example

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 //Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var LessPluginCleanCSS = require('less-plugin-clean-css'),
+    cleanCSS = new LessPluginCleanCSS({ advanced: true });
 
 gulp.task('less', function(){
     gulp.src('*.less')
@@ -9,6 +11,16 @@ gulp.task('less', function(){
             return f.base;
         }));
 });
+
+gulp.task('less-minify', function(){
+    gulp.src('*.less')
+        .pipe(less({
+            plugins: [cleanCSS]
+        }))
+        .pipe(gulp.dest(function(f){
+            return f.base;
+        }));
+})
 
 gulp.task('default', ['less'], function(){
     gulp.watch('*.less', ['less']);
